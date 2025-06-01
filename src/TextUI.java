0293417a1464e5d4077e8ce0304ddbeeacbe9378
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-06-01 19:02:11 -0400

    latest progress

--- src/TextUI.java
+++ src/TextUI.java
@@ -23,6 +23,36 @@ public class TextUI {
         }
     }
 
+    public void printToChuckNorris(String text) {
+        System.out.println("The result:");
+        for (char c : text.toCharArray()) {
+            String binary = Integer.toBinaryString(c);
+            String paddedBinary = String.format("%7s", binary).replace(' ', '0');
 
+            // loop for whole string
+            while (!paddedBinary.isEmpty()) {
+                char sequenceChar = paddedBinary.charAt(0);
+                int sequenceLength = 1;
+
+                for (int i = 1; i < paddedBinary.length(); i++) {
+                    if (paddedBinary.charAt(i) == paddedBinary.charAt(i - 1)) {
+                        sequenceLength++;
+                    } else {
+                        break;
+                    }
+                }
+
+                paddedBinary = paddedBinary.substring(1);
+
+                // loop for char sequence
+                while (!paddedBinary.isEmpty()) {
+
+
+                }
+
+            }
+
+        }
+    }
 
 }
